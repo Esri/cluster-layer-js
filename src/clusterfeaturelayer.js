493e6519882bc817e54e611d876f012b6c2d0369
David Wilton <dwilton@DWILTONW10.ECLLDN.LOCAL>
2015-11-30 03:26:04 +0000

    Print task will not print labels
    When using the esri print task with the cluster layer it will not print
    the labels because the values are set to an integer value rather than a
    string

--- src/clusterfeaturelayer.js
+++ src/clusterfeaturelayer.js
@@ -949,7 +949,7 @@ define([
             if ( label.length === 1 ) {
                 // console.log('update label...found: ', label);
                 this.remove(label[0]);
-                var newLabel = new TextSymbol(c.attributes.clusterCount)
+                var newLabel = new TextSymbol(c.attributes.clusterCount.toString())
                     .setColor(new Color(this._clusterLabelColor))
                     .setOffset(0, this._clusterLabelOffset)
                     .setFont(this._font);
